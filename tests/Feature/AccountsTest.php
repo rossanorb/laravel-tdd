c3ff2808eb79cd1a1029af7bc209b046c14da0f2
Rossano <rossanorb@gmail.com>
2018-02-21 19:38:27 -0300

    Testando listagem de dados

--- tests/Feature/AccountsTest.php
+++ tests/Feature/AccountsTest.php
@@ -9,13 +9,20 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class AccountsTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testApiList()
     {
-        $this->assertTrue(true);
+        $data = factory(\App\Account::class, 20)->create();
+
+        $response = $this->get('/api/accounts');
+        $response->assertStatus(200)->assertJson(['data'=> $data->toArray()]);
+        // $response->assertStatus(200)->assertExactJson(['data'=> $data->toArray()]);
+
     }
 }
